d593389f1cbac920cbf4a826b340dfad79e75b2c
Ian Yang <iany.yang@mail.utoronto.ca>
2019-03-08 17:00:51 -0500

    menu bar icons

--- src/components/menuBar.jsx
+++ src/components/menuBar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const MenuBar = props => {
   return (
     <div className="menu_bar">
-      <img className="menu_icon" src="https://img.icons8.com/android/24/000000/fridge.png" />
-      <img className="menu_icon align_left" src="https://img.icons8.com/dusk/64/000000/edit.png" />
-      <img className="menu_icon align_left" src="https://img.icons8.com/ios-glyphs/30/000000/beer-recipe.png" />
+      <img className="menu_icon" src="https://i.imgur.com/eusJynU.png" />
+      <img className="menu_icon align_left" src="https://i.imgur.com/zxFvvYF.png" />
+      <img className="menu_icon align_left" src="https://i.imgur.com/g6KHF1R.png" />
     </div>
   )
 }
