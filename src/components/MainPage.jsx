8b5f7237f07a199735517847b7e522521bf22c9a
Hairuo Sang <hairuo.sang@gmail.com>
2019-03-08 16:43:24 -0500

    some pages

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,22 +3,97 @@ import '../App.css';
 import MenuBar from './menuBar';
 
 class MainPage extends Component {
-  state = {}
+  constructor() {
+    super();
+    this.state = {currentState: 0}
+    this.text = [
+      "Scan a bar code to store some food!",
+      "Confirm the food item that you have scanned!",
+      "Please fill in the food that you want to add!",
+    ]
+    this.handlePhotoClick = this.handlePhotoClick.bind(this);
+    this.handleFormAdd = this.handleFormAdd.bind(this);
+    this.handleFormCancel = this.handleFormCancel.bind(this);
+
+  }
 
   render() {
     return (
       <div>
         <div className="main_page_container">
           <div className="header_text">
-            Scan a bar code to store some food!
+            {this.text[this.state.currentState]}
           </div>
-          <div className="photo_icon_container">
-            <img className="photo_icon" src="https://www.shareicon.net/data/128x128/2015/12/04/682310_cross_512x512.png" />
-          </div>
+          {this.renderMainSection()}
           <MenuBar />
         </div>
       </div>
     );
+  }
+
+  renderMainSection() {
+    console.log(this.state.currentState);
+    if (this.state.currentState == 0) {
+      return this.renderPhotoIcon();
+    }
+    else if (this.state.currentState == 1) {
+      return this.renderPopUp();
+    }
+  }
+
+  renderPhotoIcon() {
+    return (
+      <div className="photo_icon_container" onClick={this.handlePhotoClick}>
+        <img className="photo_icon" src="https://www.shareicon.net/data/128x128/2015/12/04/682310_cross_512x512.png" />
+      </div>
+    )
+  }
+
+  handlePhotoClick() {
+    this.setState({currentState: 1});
+  }
+
+  handleFormAdd() {
+    this.setState({currentState: 0});
+  }
+
+  handleFormCancel() {
+    this.setState({currentState: 0});
+  }
+
+  renderScanned() {
+
+  }
+
+  renderPopUp() {
+    return (
+      <div className="pop_up_container">
+        <div className="pop_up">
+          <form>
+            <div className="pop_up_divider">
+              Name: <input type="text" name="name" value="Carrot"/>
+              </div>
+            <div className="pop_up_divider">
+              Quantity: <input type="text" name="quantity" value="500g"/>
+            </div>
+            <div className="pop_up_divider">
+              Expiray Date: <input type="text" name="exp" value="11/19/2019"/>
+            </div>
+            <div className="pop_up_divider">
+              Storage: <input type="text" name="storage" value="Fridge"/>
+            </div>
+            <div className="form_buttons">
+              <div className="pop_up_button cancel" onClick={this.handleFormCancel}>
+                Cancel
+              </div>
+              <div className="pop_up_button add" onClick={this.handleFormAdd}>
+                Add
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
   }
 }
 
