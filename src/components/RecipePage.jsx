a2ad9d76b17d6d631806d273baa9051e10b18162
Hairuo Sang <hairuo.sang@gmail.com>
2019-03-09 22:34:24 -0500

    recipe finished

--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -8,17 +8,27 @@ class RecipePage extends Component {
     this.state = {
       filterSearch: "",
       recipes: [
-        {name: "Tomato and Egg", details: "A very nicely and detalied recipe"},
-        {name: "Minced Pork", details: "A very nicely and detalied recipe"},
+        {
+          name: "Tomato and Egg",
+          details: "A very nicely and detalied recipe",
+          filters: ["tomato", "egg"],
+          ingredients: ["tomato", "egg", "salt"]
+        },
+        {
+          name: "Minced Pork",
+          details: "A very nicely and detalied recipe",
+          filters: ["minced", "pork"],
+          ingredients: ["pork", "sauce", "rice"]
+        },
       ],
-      filters: []
+      filters: [],
+      currentState: 0,
+      currentRecipe: {},
     }
     this.handleSearchEnter = this.handleSearchEnter.bind(this);
+    this.handleMakeCancel = this.handleMakeCancel.bind(this);
+    this.handleMake = this.handleMake.bind(this);
 
-  }
-
-  handleChange(field, event) {
-    this.setState({[field]: event.target.value})
   }
 
   render() {
@@ -28,28 +38,137 @@ class RecipePage extends Component {
           <div className="recipe_header">
             Recipes
           </div>
-          <div className="recipe_filters">
-            <div className="recipe_search">
-              Filters:
-              <input
-                type="text"
-                name="name"
-                value={this.state.filterSearch}
-                onChange={(event) => {this.handleChange("filterSearch", event)}}
-                onKeyPress={this.handleSearchEnter}
-              />
-            </div>
-            <div className="filter_list">
-              {this.renderFilters()}
-            </div>
-          </div>
-          <div className="recipe_list_container">
-            {this.renderRecipes()}
-          </div>
+          {this.renderBody()}
           <MenuBar />
         </div>
       </div>
     );
+  }
+
+  renderBody() {
+    if (this.state.currentState == 0)
+      return this.renderBaseRecipePage();
+    else if (this.state.currentState == 1)
+      return this.renderViewRecipePage();
+    else if (this.state.currentState == 2)
+      return this.renderMakeRecipePage();
+  }
+
+  renderMakeRecipePage() {
+    return (
+      <div>
+        <div className="view_recipe_block">
+          <div className="recipe_title">
+            {this.state.currentRecipe.name}
+          </div>
+          <div className="pop_up_divider">
+            Make All:&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="checkbox"/>
+          </div>
+          <div className="ingredient_list">
+            {this.renderIngredients()}
+          </div>
+          <div className="recipe_buttons align_center">
+            <div className="pop_up_button" onClick={this.handleMakeCancel}>
+              Make
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderViewRecipePage() {
+    return (
+      <div>
+        <div className="view_recipe_block">
+          <div className="recipe_title">
+            {this.state.currentRecipe.name}
+          </div>
+          <div className="filter_list">
+            Filters:&nbsp;&nbsp;&nbsp;&nbsp;
+            {this.renderFilters(this.state.currentRecipe.filters)}
+          </div>
+          <div className="ingredients_header">
+            Ingredients
+          </div>
+          <div className="filter_list">
+            {this.renderFilters(this.state.currentRecipe.ingredients)}
+          </div>
+          <div className="recipe_details">
+            {this.state.currentRecipe.details}
+          </div>
+          <div className="recipe_buttons">
+            <div className="pop_up_button" onClick={this.handleMake}>
+              Make
+            </div>
+            <div className="pop_up_button" onClick={this.handleMakeCancel}>
+             Cancel
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderBaseRecipePage() {
+    return (
+      <div>
+        <div className="recipe_filters">
+          <div className="recipe_search">
+            Filters:
+            <input
+              type="text"
+              name="name"
+              value={this.state.filterSearch}
+              onChange={(event) => {this.handleChange("filterSearch", event)}}
+              onKeyPress={this.handleSearchEnter}
+            />
+          </div>
+          <div className="filter_list">
+            {this.renderFilters(this.state.filters)}
+          </div>
+        </div>
+        <div className="recipe_list_container">
+          {this.renderRecipes()}
+        </div>
+      </div>
+    )
+  }
+
+  renderIngredients() {
+    var ingredientList = []
+    this.state.currentRecipe.ingredients.map((ingredient) => {
+      ingredientList.push(
+        <div className="pop_up_divider">
+          {ingredient}:&nbsp;&nbsp;&nbsp;&nbsp;
+          <select id="select" name="ingredient" value={1}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+      )
+    })
+    return ingredientList;
+  }
+
+  handleChange(field, event) {
+    this.setState({[field]: event.target.value})
+  }
+
+  handleMakeCancel() {
+    this.setState({
+      currentState: 0
+    })
+  }
+
+  handleMake() {
+    this.setState({
+        currentState: 2
+    })
   }
 
   handleSearchEnter(event) {
@@ -61,9 +180,9 @@ class RecipePage extends Component {
     }
   }
 
-  renderFilters() {
+  renderFilters(filters) {
     var filterList = []
-    this.state.filters.map((filter) => {
+    filters.map((filter) => {
       filterList.push(
         <div className="filter_container">
           <div className="filter_text">
@@ -97,7 +216,7 @@ class RecipePage extends Component {
 
   renderRecipe(recipe) {
     return (
-      <div className="recipe_block">
+      <div className="recipe_block" onClick={(event) => this.handleRecipeClick(event, recipe)}>
         <div className="recipe_title">
           {recipe.name}
         </div>
@@ -105,6 +224,15 @@ class RecipePage extends Component {
           {recipe.details}
         </div>
       </div>
+    )
+  }
+
+  handleRecipeClick(event, recipe) {
+    this.setState(
+      {
+        currentState: 1,
+        currentRecipe: recipe,
+      }
     )
   }
 
